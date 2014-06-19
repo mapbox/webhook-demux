2d9e2046a276d69041296638d9619b5aca50df26
Tom MacWright <tom@macwright.org>
2014-06-19 14:47:33 -0400

    Simplify, remove filtering. Fix HTTPS support

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 var concat = require('concat-stream'),
     url = require('url'),
     _ = require('underscore'),
-    http = require('http');
+    http = require('http'),
+    https = require('https');
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 module.exports = function(config) {
-
     config.forEach(function(c) {
         c.parsed_url = url.parse(c.url);
-        c.matcher = _.matches(c.match);
     });
-
     function handleRequest(req, res) {
         if (req.method === 'POST' || req.method === 'PUT') {
             req.pipe(concat(function(requestBody) {
                 try {
-                    var requestData = JSON.parse(requestBody);
+                    var requestData = JSON.parse(requestBody.toString());
                     for (var i = 0; i < config.length; i++) {
-                        if (config[i].matcher(requestData)) {
-                            route(requestData, req, res, config[i]);
-                        }
+                        route(requestData, req, res, config[i]);
                     }
                 } catch(e) {
-                    console.error('could not parse json body');
+                    console.error(e);
                 } finally {
                     res.writeHead(200);
                     res.end('');
@@ -32,15 +30,38 @@ module.exports = function(config) {
             res.end('');
         }
     }
-
     function route(data, incomingRequest, response, config) {
-        var req = http.request(_.extend(config.parsed_url, {
-            headers: incomingRequest.headers,
-            method: incomingRequest.method
-        }));
-        req.write(JSON.stringify(data));
-        req.end();
-    }
+        var protocol = config.parsed_url.protocol === 'https:' ?
+            https : http;
 
+        var opts = _.extend(config.parsed_url, {
+            headers: {
+                'content-type': 'application/json',
+                'content-length': JSON.stringify(data).length
+            },
+            method: incomingRequest.method,
+            port: protocol === https ? 443 : 80
+        });
+
+        var req = protocol.request(opts);
+
+        req.on('response', function(res) {
+            var body = '';
+            res.on('data', function(chunk) {
+                console.log('got data');
+                body += chunk.toString();
+            });
+            res.on('end', function() {
+                console.log('done');
+                console.log(body);
+            });
+        });
+
+        req.on('error', function(e) {
+            console.log(e);
+        });
+
+        req.end(JSON.stringify(data));
+    }
     return handleRequest;
 };
